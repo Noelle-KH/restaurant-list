90db74e29d2fdeebaa8e36606562157655529383
Noelle-KH <noelletang86@gmail.com>
2022-11-14 11:50:43 +0800

    feat: add update restaurant function

--- app.js
+++ app.js
@@ -55,6 +55,33 @@ app.get('/restaurants/:id', (req, res) => {
     .catch(error => console.log(error))
 })
 
+app.get('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then(restaurant => res.render('edit', { restaurant }))
+    .catch(error => console.log(error))
+})
+
+app.post('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  const [name, category, image, location, phone, google_map, rating, description] = [req.body.name, req.body.category, req.body.image, req.body.location, req.body.phone, req.body.google_map, Number(req.body.rating), req.body.description]
+  return Restaurant.findById(id)
+    .then(restaurant => {
+      restaurant.name = name
+      restaurant.category = category
+      restaurant.image = image
+      restaurant.location = location
+      restaurant.phone = phone
+      restaurant.google_map = google_map
+      restaurant.rating = rating
+      restaurant.description = description
+      return restaurant.save()
+    })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword.trim()
   const searchResult = restaurants.results.filter(restaurant => {
