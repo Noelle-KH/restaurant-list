399dface90d4ebe4edd80b9437c42129c71010dd
Noelle-KH <noelletang86@gmail.com>
2022-12-23 15:34:23 +0800

    refactor: combine search and sort function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const Restaurant = require('../../models/restaurant-model')
 
 router.get('/', (req, res) => {
+  const keyword = (req.query.keyword) ? req.query.keyword.trim() : ''
   const sort = req.query.sort || 'default'
   const sortBy = {
     default: { _id: 'asc' },
@@ -16,7 +17,17 @@ router.get('/', (req, res) => {
   Restaurant.find()
     .lean()
     .sort(sortBy[sort])
-    .then(restaurants => res.render('index', { restaurants, sortSelected }))
+    .then(restaurants => {
+      const searchResult = restaurants.filter(restaurant => restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.includes(keyword))
+
+      if (!keyword) {
+        res.render('index', { restaurants, sortSelected })
+      } else if (!searchResult.length) {
+        res.render('not-found', { keyword })
+      } else {
+        res.render('index', { restaurants: searchResult, keyword, sortSelected })
+      }
+    })
     .catch(error => console.log(error))
 })
 
