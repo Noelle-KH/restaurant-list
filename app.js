349e0de645e9c43c56fa8590f04a50b94a8f83a6
Noelle-KH <noelletang86@gmail.com>
2023-02-03 08:00:57 +0800

    refactor: modify import method and remove comment

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
-// 載入Express、樣板引擎、body-parser
 const express = require('express')
 const exphbs = require('express-handlebars')
-const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 
 const port = 3000
 const app = express()
 
-//! 重構路由統一封裝於routes、資料庫連線設定於config
 const routes = require('./routes')
 require('./config/mongoose')
 
-// 設定樣板引擎、靜態檔案及body-parser、路由
 app.engine('handlebars', exphbs({
   defaultLayout: 'main',
   helpers: {
@@ -22,11 +18,11 @@ app.engine('handlebars', exphbs({
 }))
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(routes)
 
-// 啟動與監聽伺服器
+
 app.listen(port, () => {
   console.log(`Listening on http://localhost:${port}`)
 })
