0a2af846a8e81efee68f9a50f6ce63650b8a3186
Noelle-KH <noelletang86@gmail.com>
2023-02-03 08:52:24 +0800

    feat: add user register feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,8 +1,38 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../../models/user-model')
 
 router.get('/login', (req, res) => {
   res.render('login')
+})
+
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  if (!name || !email || !password || !confirmPassword) {
+    console.log('所有欄位都是必填')
+    return res.render('register', {
+      name, email, password, confirmPassword
+    })
+  }
+  if (password !== confirmPassword) {
+    console.log('密碼與確認密碼不相符')
+    return res.render('register', {
+      name, email, password, confirmPassword
+    })
+  }
+  User.findOne({ email })
+    .then(user => {
+      if (user) {
+      }
+      return User.create({ name, email, password })
+        .then(() => res.redirect('/'))
+        .catch(error => console.log(error))
+    })
+    .catch(error => console.log(error))
 })
 
 module.exports = router
