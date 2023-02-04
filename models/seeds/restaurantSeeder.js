b01490cd0df21b4ca26d27be661430941d7ab261
Noelle-KH <noelletang86@gmail.com>
2023-02-04 09:29:38 +0800

    feat: modify seeder

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,10 +1,27 @@
-//! 重構種子資料內的資料庫連線設定
 const db = require('../../config/mongoose')
-const restaurants = require('./restaurant.json').results
+const restaurantData = require('./restaurant.json').results
+const userData = require('./user.json').results
 const Restaurant = require('../restaurant-model')
+const User = require('../user-model')
 
 db.once('open', () => {
-  Restaurant.insertMany(restaurants)
-  console.log('done')
+  return Promise.all(userData.map(user => {
+    const { email, password, restaurantIndex } = user
+    const name = email.slice(0, email.indexOf('@'))
+    return User.create({ name, email, password })
+      .then(user => {
+        const restaurants = restaurantIndex.map(index => {
+          const restaurant = restaurantData[index]
+          restaurant.userId = user._id
+          return restaurant
+        })
+        return Restaurant.create(restaurants)
+      })
+  }))
+    .then(() => {
+      console.log('done')
+      process.exit()
+    })
+    .catch(error => console.log(error))
 })
 
