b57e8a4602f3b776ea542be1030edca1d1d31ec8
Noelle-KH <noelletang86@gmail.com>
2023-02-06 07:08:54 +0800

    feat: add google login

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,7 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
 const FacebookStrategy = require('passport-facebook').Strategy
+const GoogleStrategy = require('passport-google-oauth2').Strategy
 const bcrypt = require('bcryptjs')
 const User = require('../models/user-model')
 
@@ -9,7 +10,7 @@ module.exports = app => {
   app.use(passport.session())
 
   passport.use(new LocalStrategy({ usernameField: 'email', passReqToCallback: true }, (req, email, password, done) => {
-    
+
     return User.findOne({ email })
       .then(user => {
         if (!user) return done(null, false, req.flash('warning_message', '此信箱尚未註冊'))
@@ -27,17 +28,13 @@ module.exports = app => {
     clientSecret: process.env.FACEBOOK_SECRET,
     callbackURL: process.env.FACEBOOK_CALLBACK,
     profileFields: ['email', 'displayName']
-  }, (accessToken, refreshToken, profile, done) => {
-    const { name, email } = profile._json
-    return User.findOne({ email })
-      .then(user => {
-        if (user) return done(null, user)
-        const randomPassword = Math.random().toString(36).slice(-8)
-        return User.create({ name, email, password: bcrypt.hashSync(randomPassword, bcrypt.genSaltSync(10)) })
-          .then(user => done(null, user))
-      })
-      .catch(error => done(error))
-  }))
+  }, verifyCallback))
+
+  passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_ID,
+    clientSecret: process.env.GOOGLE_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK
+  }, verifyCallback))
 
   passport.serializeUser((user, done) => {
     done(null, user.id)
@@ -50,3 +47,15 @@ module.exports = app => {
   })
 }
 
+
+function verifyCallback(accessToken, refreshToken, profile, done) {
+  const { name, email } = profile._json
+  return User.findOne({ email })
+    .then(user => {
+      if (user) return done(null, user)
+      const randomPassword = Math.random().toString(36).slice(-8)
+      return User.create({ name, email, password: bcrypt.hashSync(randomPassword, bcrypt.genSaltSync(10)) })
+        .then(user => done(null, user))
+    })
+    .catch(error => done(error))
+}
